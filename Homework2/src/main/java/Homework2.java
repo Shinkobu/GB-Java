0a2d98392d2d279c81b7536cee12176300df5b41
Alexandr Sinkov <sinkov86@yandex.ru>
2022-06-30 20:52:56 +0300

    Task1 - array sorting added

--- Homework2/src/main/java/Homework2.java
+++ Homework2/src/main/java/Homework2.java
@@ -1,15 +1,30 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Homework2 {
     public static void main(String[] args) {
 
+//    У вас есть отсортированный массив
+//    нужно используя минимальное количество попыток найти загаданное число (Алгоритм бинарного поиска)
 
         Scanner myScan = new Scanner(System.in);
-        int sortedArray[]; // объявление массива
-//     sortedArray = new int[10]; // выделение памяти для массива
-        sortedArray = new int[] {1,2,3,4,5,6,7,8,9,10};
-        System.out.println("Дан массив: ");
+        int rawArray[]; // объявление массива
+        rawArray = new int[10]; // выделение памяти для массива
+//        sortedArray = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+        // Заполняем массив случайными числами
+        for (int i = 0; i < rawArray.length ; i++) {
+            rawArray[i] = (int) (Math.random()*100); // Диапазон [0;1) умножается на 100. ( Math.random() * (b-a) ) + a, при интервале (a;b]
+        }
+        System.out.println("Сгенерирован массив: ");
+        System.out.println(Arrays.toString(rawArray));
+
+        // сортировка
+        int [] sortedArray = shuttleSort(rawArray);
+
+
+        System.out.println("\nОтсортированный массив: ");
         System.out.println(Arrays.toString(sortedArray));
         System.out.printf("\nВведите искомое число: ");
 
@@ -18,25 +33,22 @@ public class Homework2 {
             System.out.printf("Ошибка ввода. Требуется ввести целое число: ");
             myScan.next();
         }
-
-
         int digitToFind = myScan.nextInt();
-
+        myScan.close();
 
 
         Task1(sortedArray,digitToFind,0,sortedArray.length-1);
 
     }
 
-//    У вас есть отсортированный массив
-//    нужно используя минимальное количество попыток найти загаданное число (Алгоритм бинарного поиска )
+
 
     public static void Task1(int[] sortedArray, int digitToFind, int firstIndex, int lastIndex ) {
 
      int targetIndex = (firstIndex+lastIndex) / 2;
 
      if (firstIndex==lastIndex){
-         System.out.println("\nОшибка. Число не найдено\n");
+         System.out.println("\nЧисло не найдено\n");
          return;
      }
      if (sortedArray[targetIndex] == digitToFind) {
@@ -56,4 +68,25 @@ public class Homework2 {
 
     }
 
+    public static int[] shuttleSort(int[] array) {
+        int temp;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                temp = array[i];
+                array[i] = array [i-1];
+                array[i-1] = temp;
+
+                for (int z = i - 1; (z - 1) >= 0; z--) {
+                    if (array[z] < array[z - 1]) {
+                        temp = array[z];
+                        array[z] = array [z-1];
+                        array[z-1] = temp;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
 }
