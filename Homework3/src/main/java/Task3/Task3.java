28582084b22f6e6e21766c275c6453172f91e558
Alexandr Sinkov <sinkov86@yandex.ru>
2022-07-16 01:54:18 +0300

    encode changes

--- Homework3/src/main/java/Task3/Task3.java
+++ Homework3/src/main/java/Task3/Task3.java
@@ -2,21 +2,21 @@ package Task3;
 
 public class Task3 {
     public static void main(String[] args) {
-//        Написать программу, показывающую последовательность действий для игры “Ханойская башня”
+//        РќР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ, РїРѕРєР°Р·С‹РІР°СЋС‰СѓСЋ РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚СЊ РґРµР№СЃС‚РІРёР№ РґР»СЏ РёРіСЂС‹ вЂњРҐР°РЅРѕР№СЃРєР°СЏ Р±Р°С€РЅСЏвЂќ
 
-        int n = 5; // Количество дисков
-        towerOfHanoi(n, 'A', 'C', 'B');  // A, B и C - названия стержней
+        int n = 5; // РљРѕР»РёС‡РµСЃС‚РІРѕ РґРёСЃРєРѕРІ
+        towerOfHanoi(n, 'A', 'C', 'B');  // A, B Рё C - РЅР°Р·РІР°РЅРёСЏ СЃС‚РµСЂР¶РЅРµР№
 
     }
         static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod){
 
             if (n == 1){
-                System.out.println("Переместить диск 1 с башни " +  from_rod + " на башню " + to_rod);
+                System.out.println("РџРµСЂРµРјРµСЃС‚РёС‚СЊ РґРёСЃРє 1 СЃ Р±Р°С€РЅРё " +  from_rod + " РЅР° Р±Р°С€РЅСЋ " + to_rod);
                 return;
             }
 
             towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
-            System.out.println("Переместить диск " + n + " с башни " +  from_rod + " на башню " + to_rod);
+            System.out.println("РџРµСЂРµРјРµСЃС‚РёС‚СЊ РґРёСЃРє " + n + " СЃ Р±Р°С€РЅРё " +  from_rod + " РЅР° Р±Р°С€РЅСЋ " + to_rod);
             towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
         }
 
