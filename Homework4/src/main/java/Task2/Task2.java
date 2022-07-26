2e4609f34117bdec84a73da4dea0526c74d15547
Alexandr Sinkov <sinkov86@yandex.ru>
2022-07-27 00:03:51 +0300

    task 2 in progress

--- Homework4/src/main/java/Task2/Task2.java
+++ Homework4/src/main/java/Task2/Task2.java
@@ -42,11 +42,29 @@ public class Task2 {
 
         System.out.println("\nИмеем массив с индексами: \n" + Arrays.toString(indexArr));
 
-//      Очередь задач
+//      Очередь задач, её заполнение
 
         Deque<Tasks> myQueue = new LinkedList<Tasks>();
 
         for (int i = 0; i < indexArr.length; i++) {
+
+//          Определение приоритета попадания в очередь
+
+            try {
+                Integer d1 = indexArr[i];
+                Integer d2 = indexArr[i+1];
+
+                if (d1 < d2){
+                    Integer tempInt = indexArr[i];
+                    indexArr[i] = indexArr[i+1];
+                    indexArr[i+1] = tempInt;
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+
+            }
+
+            System.out.println("Расстановка с учётом приоритета: " + Arrays.toString(indexArr));
+
             myQueue.add(tasksList.get(indexArr[i]));
         }
         System.out.println("\nИмеем очередь заданий: \n" + myQueue);
@@ -61,12 +79,16 @@ public class Task2 {
         while (myQueue.size() != 0){
 
             Iterator<Tasks> myIterator = myQueue.iterator();
-            String showStr = "";
-            while (myIterator.hasNext()){
-                showStr += myIterator.next().name() + " / ";
-            }
+            Iterator<Tasks> myIterator1 = myQueue.iterator();
+            String showName = "";
+            String showPrior = "";
+            while (myIterator.hasNext()) showName += myIterator.next().name() + " / ";
+            while (myIterator1.hasNext()) showPrior += myIterator1.next().prioritet() + " / ";
 
-            System.out.println("Шаг "+s+". Очередь имеет вид: " + showStr);
+
+            System.out.println("Шаг "+s+". Очередь имеет вид: " + showName);
+            System.out.println("       Приоритет: " + showPrior);
+
             myQueue.pollFirst();
             s++;
 
